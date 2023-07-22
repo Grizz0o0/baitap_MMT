4a3f6679df644212dcb2811a0e7e110ef985cf00
Hong Ky <vuonghongky26@gmail.com>
2023-07-22 23:57:05 +0700

    fix countdown

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,15 +14,6 @@ const modalCloseFunc = function () {
 modalCloseOverlay.addEventListener("click", modalCloseFunc);
 modalCloseBtn.addEventListener("click", modalCloseFunc);
 
-// // notification toast variables
-// const notificationToast = document.querySelector("[data-toast]");
-// const toastCloseBtn = document.querySelector("[data-toast-close]");
-
-// // notification toast eventListener
-// toastCloseBtn.addEventListener("click", function () {
-//     notificationToast.classList.add("closed");
-// });
-
 // mobile menu variables
 const mobileMenuOpenBtn = document.querySelectorAll(
     "[data-mobile-menu-open-btn]"
@@ -72,58 +63,55 @@ for (let i = 0; i < accordionBtn.length; i++) {
 }
 
 // Count down
-const discount = {
-    day: 360,
-    hours: 24,
-    min: 60,
-    sec: 60,
-};
+function updateCountdown(countdownElement, targetDate) {
+    const now = new Date().getTime();
+    const timeLeft = targetDate - now;
 
-function countDown({ day, hours, min, sec }) {
-    let appendDay = document.querySelector("#day");
-    let appendHours = document.querySelector("#hours");
-    let appendMin = document.querySelector("#min");
-    let appendSec = document.querySelector("#sec");
+    if (timeLeft < 0) {
+        // If the target date has passed, display 0 for all countdown elements
+        countdownElement
+            .querySelectorAll(".display-number")
+            .forEach((element) => {
+                element.textContent = "0";
+            });
+        return;
+    }
 
-    const endDate = new Date().getTime() + day * hours * min * sec * 1000;
+    // Calculate the days, hours, minutes, and seconds
+    const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+    const hours = Math.floor(
+        (timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
 
-    const updateTimer = setInterval(() => {
-        const now = new Date().getTime();
-        const timeRemaining = endDate - now;
-
-        const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
-
-        const hours = Math.floor(
-            (timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-        );
-
-        const minutes = Math.floor(
-            (timeRemaining % (1000 * 60 * 60)) / (1000 * 60)
-        );
-        const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
-
-        appendDay.innerText = days;
-        appendHours.innerText = hours;
-        appendMin.innerText = minutes;
-        appendSec.innerText = seconds;
-
-        if (timeRemaining < 0) {
-            clearInterval(updateTimer);
-            appendDay.innerText = "0";
-            appendHours.innerText = "0";
-            appendMin.innerText = "0";
-            appendSec.innerText = "0";
-        }
-    }, 1000);
+    // Update the countdown elements with the calculated values
+    countdownElement.querySelector(".day.display-number").textContent = days;
+    countdownElement.querySelector(".hours.display-number").textContent = hours;
+    countdownElement.querySelector(".min.display-number").textContent = minutes;
+    countdownElement.querySelector(".sec.display-number").textContent = seconds;
 }
 
-countDown({ ...discount });
+// Set the target date for each countdown (change these to your desired dates)
+const countdownElements = document.querySelectorAll(".countdown");
+const targetDates = [
+    new Date("2023-08-01 00:00:00").getTime(),
+    new Date("2023-09-15 12:00:00").getTime(),
+    new Date("2023-12-31 23:59:59").getTime(),
+];
 
-// Lặp qua các khối mã HTML và gọi hàm countDown cho từng khối
-const countdownBlocks = document.querySelectorAll(".countdown-block");
-countdownBlocks.forEach((block) => {
-    countDown(block, { ...discount });
-});
+// Function to update all countdowns
+function updateAllCountdowns() {
+    for (let i = 0; i < countdownElements.length; i++) {
+        updateCountdown(countdownElements[i], targetDates[i]);
+    }
+}
+
+// Initial update
+updateAllCountdowns();
+
+// Update all countdowns every second (1000 milliseconds)
+setInterval(updateAllCountdowns, 1000);
 
 //open Modal
 
